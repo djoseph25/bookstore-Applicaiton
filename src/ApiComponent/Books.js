00266f490ffaa38060f1d63a125d3e8b14c7060d
djoseph25 <59910157+djoseph25@users.noreply.github.com>
2020-12-28 08:17:45 -0500

    update books project

--- src/ApiComponent/Books.js
+++ src/ApiComponent/Books.js
@@ -32,7 +32,7 @@ class Books extends Component {
 							<Row>
 								<Col lg={5} md={5} sm={5} xs={5}>
 									<img src={this.props.image} className="profile-image" alt="bookImage" />
-									{this.props.id}
+									{/* {this.props.id} */}
 								</Col>
 								<Col lg={7} md={7} sm={7} xs={7}>
 									<div className="Book-info">
